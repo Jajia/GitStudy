ac66991f9bc72036029d14e648cbd8bcbc7414af
Jajia <18351602146@163.com>
2019-08-18 19:00:00 +0800

    [ADD] print hello world

--- src/main/java/sean/learngit/controller/HelloWorldController.java
+++ src/main/java/sean/learngit/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String hello() {
+        System.out.println("hello world");
         return "hello";
     }
 }
